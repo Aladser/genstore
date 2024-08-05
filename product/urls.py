4ce39b927c4f099bb39336077a5099d0dcf29e24
Aladser <Aladser@users.noreply.github.com>
2024-08-05 16:31:29 +0900

    добавлен кэш product_list, product_detail

--- product/urls.py
+++ product/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
+
 from product.views import *
 from product.apps import ProductConfig
 
 app_name = ProductConfig.name
 
 urlpatterns = [
-    path('', ProductListView.as_view(), name='list'),
-    path('detail/<int:pk>',ProductDetailView.as_view(), name='detail'),
+    path('', cache_page(300)(ProductListView.as_view()), name='list'),
+    path('detail/<int:pk>', cache_page(60)(ProductDetailView.as_view()), name='detail'),
     path('create/', ProductCreateView.as_view(), name='create'),
     path('edit/<int:pk>', ProductUpdateView.as_view(), name='edit'),
     path('product/delete/<int:pk>', ProductDeleteView.as_view(), name='delete'),
