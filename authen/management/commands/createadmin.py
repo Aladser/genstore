595ba048eb07d2802f36585ecd6c306fafde278b
Aladser <Aladser@users.noreply.github.com>
2024-08-23 15:47:52 +0900

    РФКТ

--- authen/management/commands/createadmin.py
+++ authen/management/commands/createadmin.py
@@ -1,32 +1,30 @@
 from django.core.management import BaseCommand
+from django.shortcuts import get_object_or_404
 
 from authen.models import User, Country
+from libs.seeding import Seeding
 
 
 class Command(BaseCommand):
+    country_obj_list = [
+        {'pk': 1, 'name': 'russia', 'description': 'Россия'},
+        {'pk': 2, 'name': 'ukraine', 'description': 'Украина'},
+        {'pk': 3, 'name': 'belarus', 'description': 'Беларусь'},
+        {'pk': 4, 'name': 'kazakhstan', 'description': 'Казахстан'},
+        {'pk': 5, 'name': 'armenia', 'description': 'Армения'},
+    ]
+    user_dict = [
+        {
+            'email': 'admin@test.ru',
+            'first_name': 'Админ',
+            'last_name': 'Админов',
+            'is_superuser': True,
+            'is_staff': True
+        }
+    ]
+    password = '__strongpassword__'
+
     def handle(self, *args, **kwargs):
-        country_obj_list = [
-            {'pk': 1, 'name': 'russia', 'description': 'Россия'},
-            {'pk': 2, 'name': 'ukraine', 'description': 'Украина'},
-            {'pk': 3, 'name': 'belarus', 'description': 'Беларусь'},
-            {'pk': 4, 'name': 'kazakhstan', 'description': 'Казахстан'},
-            {'pk': 5, 'name': 'armenia', 'description': 'Армения'},
-        ]
-
-        Country.objects.all().delete()
-        Country.objects.bulk_create([Country(**param) for param in country_obj_list])
-        russia_model = Country.objects.get(name='russia')
-
-        User.objects.all().delete()
-        user = User.objects.create(
-            email='admin@test.ru',
-            country=russia_model,
-            first_name='Админ',
-            last_name='Админов',
-            is_superuser=True,
-            is_staff=True
-        )
-
-        user.set_password("admin@123")
-        user.save()
-
+        Seeding.seed_table(Country, self.country_obj_list)
+        self.user_dict[0]['country'] = get_object_or_404(Country, name='russia')
+        Seeding.seed_users(User, self.user_dict, self.password)
